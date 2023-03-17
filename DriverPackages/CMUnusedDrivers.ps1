e18a7a08339228efe6bf884b19ac233142f16fa5
SHISHIR KUSHAWAHA <srktcg@gmail.com>
2023-03-17 21:06:10 +0530

    Update CMUnusedDrivers.ps1

--- DriverPackages/CMUnusedDrivers.ps1
+++ DriverPackages/CMUnusedDrivers.ps1
@@ -1,19 +1,29 @@
 
 
-$drivers= (get-cmdriver -fast).ci_id
+$drivers= get-cmdriver -fast
+$driverIDs= $drivers.ci_id
+
+#list of driver packages
 $driverpackageid=Get-CMDriverPackage -Fast | Select-Object packageid
 
-$unusedDrivers=@()
+#list of drivers integrated in boot image
+$bootimageDriversIDs=(Get-CMBootImage | Select-Object -ExpandProperty ReferencedDrivers).id
+
+$unusedDrivers=$null
 $usedDrivers=@()
+
+#list drivers from all driver packages
 foreach($pkgid in $driverpackageid)
 {
-    write-host ""
     write-host "Checking driver package $pkgid."
     $usedDrivers+=(Get-CMDriver -Fast -DriverPackageId $pkgid).ci_id
-    $usedDrivers+=(Get-CMBootImage | Select-Object -ExpandProperty ReferencedDrivers).id
 }
+#Add boot image drivers in driver packages drivers
+$usedDrivers+=$bootimageDriversIDs
 
+#Remove duplicate drivers
 $usedDrivers=$usedDrivers | Select-Object -Unique
 
-$unusedDrivers =  Compare-Object -ReferenceObject $usedDrivers -DifferenceObject $drivers -PassThru
+#Subtract used drivers from all drivers to get the list if unused drivers
+$unusedDrivers =  Compare-Object -ReferenceObject $usedDrivers -DifferenceObject $driverIDs -PassThru
 
