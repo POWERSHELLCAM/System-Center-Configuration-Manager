fce3575c433eafc9f4597ae2f8e28e58d04f7d33
SHISHIR KUSHAWAHA <srktcg@gmail.com>
2023-03-21 12:00:41 +0530

    Update DriversHavingDeletedSource.ps1

--- DriverPackages/DriversHavingDeletedSource.ps1
+++ DriverPackages/DriversHavingDeletedSource.ps1
@@ -1,54 +1,51 @@
 function InitializeSCCM 
 { 
-    $ProcessMessage="`n Please wait.Initializing SCCM ........." 
-    
-    # Site configuration
-    do
-    { 
-        write-host "`n Enter Site Code : " -foregroundcolor $inputcolor -nonewline 
-        $SiteCode = read-host 
-        $siteResult=($siteCode -match '\b^[a-zA-Z0-9]{3}\b')
-        if(!$siteResult)
-        {
-        write-host " Site code can have only [3] alphanumeric characters. Please re-enter site code" -foregroundcolor RED
-        }
-    }while(!$siteResult)
+$ProcessMessage="`n Please wait.Initializing SCCM ........." 
+ 
+# Site configuration
+do
+{ 
+	write-host "`n Enter Site Code : " -foregroundcolor $inputcolor -nonewline 
+	$SiteCode = read-host 
+	$siteResult=($siteCode -match '\b^[a-zA-Z0-9]{3}\b')
+	if(!$siteResult)
+	{
+	write-host " Site code can have only [3] alphanumeric characters. Please re-enter site code" -foregroundcolor RED
+	}
+}while(!$siteResult)
 
-    do
-    {
-        write-host "`n Enter SMS Provider Server Name : " -foregroundcolor $inputcolor -nonewline 
-        $ProviderMachineName = read-host 
-        $nameResult=($ProviderMachineName -match '\b^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$\b')
-        if(!$nameResult)
-        {
-        write-host " Entered SMS provider name is not valid as per naming conventions. Please re-enter provider name" -foregroundcolor RED
-        }
-    }while(!$nameResult)
+do
+{
+	write-host "`n Enter SMS Provider Server Name : " -foregroundcolor $inputcolor -nonewline 
+	$ProviderMachineName = read-host 
+	$nameResult=($ProviderMachineName -match '\b^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$\b')
+	if(!$nameResult)
+	{
+	write-host " Entered SMS provider name is not valid as per naming conventions. Please re-enter provider name" -foregroundcolor RED
+	}
+}while(!$nameResult)
 
-    Invoke-Expression $ProcessColor 
-    Start-Sleep 2 
-    # Customizations 
-    $initParams = @{} 
-    
-    # Import the ConfigurationManager.psd1 module  
-    if($null -eq (Get-Module ConfigurationManager)) 
-    { 
-        Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams  
-    } 
-    
-    # Connect to the site's drive if it is not already present 
-    if($null -eq (Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue)) 
-    { 
-        New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams 
-    } 
-    
-    # Set the current location to be the site code. 
-    Set-Location "$($SiteCode):\" @initParams 
+Invoke-Expression $ProcessColor 
+Start-Sleep 2 
+# Customizations 
+$initParams = @{} 
+ 
+# Import the ConfigurationManager.psd1 module  
+if($null -eq (Get-Module ConfigurationManager)) { 
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams  
+} 
+ 
+# Connect to the site's drive if it is not already present 
+if($null -eq (Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue)) { 
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams 
+} 
+ 
+# Set the current location to be the site code. 
+Set-Location "$($SiteCode):\" @initParams 
 } 
  
 function deinitializeSCCM 
-{
-    #De-initialize SCCM
+{ 
 	$ProcessMessage="`n Please wait.De-Initializing SCCM ......" 
 	Invoke-Expression $ProcessColor 
 	Start-Sleep 2 
@@ -56,13 +53,15 @@ function deinitializeSCCM
 } 
 function updateHTML
 {
-    param ($strPath)
-    if(Test-Path $strPath)
-    { 
-        #Delete .HTML file if already exists
-        Remove-Item $strPath
-    }
-}
+
+param ($strPath)
+IF(Test-Path $strPath)
+  { 
+   Remove-Item $strPath
+   
+  }
+
+ }
  
 #--CSS formatting
 $test=@'
@@ -78,13 +77,14 @@ tr:nth-child(odd) { background: #b8d1f3; }
 '@
 
 #--Variable declaration
-Clear-Host
-$location=get-location 
-$InputColor="yellow" 
-$ProcessColor="write-host `$ProcessMessage -ForegroundColor gray -BackgroundColor darkgreen" 
-$ReportTitle="DRIVERS WITH NO DATA SOURCE"
-$strPath = "$location\$ReportTitle.html" 
-updateHTML $strPath
+ Clear-Host
+ $location=get-location 
+ $InputColor="yellow" 
+ $ProcessColor="write-host `$ProcessMessage -ForegroundColor gray -BackgroundColor darkgreen" 
+ $ReportTitle="DRIVERS WITH NO DATA SOURCE"
+ $strPath = "$location\$ReportTitle.html" 
+ updateHTML $strPath
+
 
 ConvertTo-Html -Head $test -Title $ReportTitle -Body "<h1> DRIVER WITH NO DATA SOURCE </h1>" >  "$strPath"
 InitializeSCCM 
@@ -101,27 +101,30 @@ foreach($c in $driverInfo)
     $e=$null
     $($c.CONTENTSOURCEPATH)
     $r=Test-Path -LiteralPath "$($c.CONTENTSOURCEPATH)" -ErrorVariable e
-    if($e.count -eq 0)
+    if($null -eq $e -or $e -eq "" -or $e.count -eq 0)
     {
-        $m="Path does not exist"
-    }
-    else
-    {
+        $m="Path does not exists."
+     }
+     else
+     {
         $m=$($e -split '`n')
-    }
+      }
     if($r -eq $false)
     {
-        $property=$null
-        $property=$c |Select-Object ci_id,LocalizedDisplayName,CONTENTSOURCEPATH
-        $newProperty = [ordered]@{}
-        $newProperty."SrNo" = $i+1
-        $newProperty."CI_ID" = $property.ci_id
-        $newProperty."Name" = $property.LocalizedDisplayName
-        $newProperty."SourcePath" = $property.CONTENTSOURCEPATH
-        $newProperty."Result" = $m
-        $Objectname = New-Object PSobject -Property $newProperty
-        $result += $Objectname
-        $i++
+        if($m -notmatch 'Access is denied')
+        {
+            $property=$null
+            $property=$c |Select-Object ci_id,LocalizedDisplayName,CONTENTSOURCEPATH
+            $newProperty = [ordered]@{}
+            $newProperty."Sr No" = $i+1
+            $newProperty."CI_ID" = $property.ci_id
+            $newProperty."Name" = $property.LocalizedDisplayName
+            $newProperty."Source Path" = $property.CONTENTSOURCEPATH
+            $newProperty."Result" =  $m  
+            $Objectname = New-Object PSobject -Property $newProperty
+            $result += $Objectname
+            $i++
+        }
     }
 }
 $count=$driverInfo.count
