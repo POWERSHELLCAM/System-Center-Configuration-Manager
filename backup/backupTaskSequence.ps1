5ad37c7f3dc7632f7f489ac2c7d9712d0ca81082
SHISHIR KUSHAWAHA <srktcg@gmail.com>
2023-04-03 17:08:37 +0530

    Update backupTaskSequence.ps1

--- backup/backupTaskSequence.ps1
+++ backup/backupTaskSequence.ps1
@@ -86,7 +86,7 @@ if($BackupPath -ne "")
     # Export the task sequences to location mentioned
     $AllTaskSequences | ForEach-Object { 
         "Exporting Task Sequence $($_.name)-$($_.packageid)"
-        Export-CMTaskSequence -TaskSequencePackageId $_.packageid -ExportFilePath "$BackupFolder\$($_.name).zip" -WithContent $false -WithDependence $false
+        Export-CMTaskSequence -TaskSequencePackageId $_.packageid -ExportFilePath "$BackupFolder\$($_.packageid)-$($_.name).zip" -WithContent $false -WithDependence $false
     }
     deinitializeSCCM
 }
